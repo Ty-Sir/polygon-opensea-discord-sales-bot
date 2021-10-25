00509725b36295b0bcdf191dcc06cff0a3a4c646
Tyler Siravo <tylersiravo@gmail.com>
2021-10-25 16:16:48 -0400

    initial commit

---
+++ checkSales.ts
@@ -0,0 +1,74 @@
+import 'dotenv/config';
+import Discord, { TextChannel } from 'discord.js';
+import fetch from 'node-fetch';
+import { ethers } from "ethers";
+
+const COLLECTION_SLUG = 'cookiethedog';
+const DISCORD_BOT_TOKEN = 'OTAyMjgxMTE3MzgwNTE4MDA5.YXcIxQ.q3BJGcm-hpigOwg1QKwsBTD6mrs';
+const DISCORD_CHANNEL_ID = '901146130254020658'
+
+const discordBot = new Discord.Client();
+const  discordSetup = async (): Promise<TextChannel> => {
+  return new Promise<TextChannel>((resolve, reject) => {
+    if (!DISCORD_BOT_TOKEN) reject(`${DISCORD_BOT_TOKEN} not set`);
+    if (!DISCORD_CHANNEL_ID) reject(`${DISCORD_CHANNEL_ID} not set`);
+  
+    discordBot.login(DISCORD_BOT_TOKEN);
+    discordBot.on('ready', async () => {
+      const channel = await discordBot.channels.fetch(DISCORD_CHANNEL_ID);
+      resolve(channel as TextChannel);
+    });
+  })
+}
+
+const buildMessage = (sale: any) => (
+  new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle(sale.asset.name + ' sold!')
+	.setURL(sale.asset.permalink)
+	.setAuthor('OpenSea Bot', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png', 'https://github.com/sbauch/opensea-discord-bot')
+	.setThumbnail(sale.asset.collection.image_url)
+	.addFields(
+		{ name: 'Name', value: sale.asset.name },
+		{ name: 'Amount', value: `${ethers.utils.formatEther(sale.total_price || '0')}${ethers.constants.EtherSymbol}`},
+		{ name: 'Buyer', value: sale?.winner_account?.address, },
+		{ name: 'Seller', value: sale?.seller?.address,  },
+	)
+  .setImage(sale.asset.image_url)
+	.setTimestamp(Date.parse(`${sale?.created_date}Z`))
+	.setFooter('Sold on OpenSea', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png')
+)
+
+async function main() {
+  const channel = await discordSetup();
+  const seconds = process.env.SECONDS ? parseInt(process.env.SECONDS) : 3_600;
+  const hoursAgo = (Math.round(new Date().getTime() / 1000) - (seconds)); // in the last hour, run hourly?
+  
+  const params = new URLSearchParams({
+    offset: '0',
+    event_type: 'successful',
+    only_opensea: 'false',
+    occurred_after: hoursAgo.toString(), 
+    collection_slug: COLLECTION_SLUG,
+  })
+
+  const openSeaResponse = await fetch(
+    "https://api.opensea.io/api/v1/events?" + params).then((resp) => resp.json());
+    
+  return await Promise.all(
+    openSeaResponse?.asset_events?.reverse().map(async (sale: any) => {
+      const message = buildMessage(sale);
+      return channel.send(message)
+    })
+  );   
+}
+
+main()
+  .then((res) =>{ 
+    if (!res.length) console.log("No recent sales")
+    process.exit(0)
+  })
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
